55c2a4fc77dbf90b9ae4debd61f133d36b17d194
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2024-11-20 21:13:27 +0530

    arrays remain

--- src/LeetCodeProblemLinksSolved/Links.java
+++ src/LeetCodeProblemLinksSolved/Links.java
@@ -2,8 +2,13 @@ package LeetCodeProblemLinksSolved;
 
 public class Links {
     public static void main(String[] args) {
+        /*
+        to optimize this problems in future
+         */
         //https://leetcode.com/problems/two-sum/ -->SAP_year1(EASY)
         //https://leetcode.com/problems/fibonacci-number -->SAP_year1(EASY)
         //https://leetcode.com/problems/best-time-to-buy-and-sell-stock -->SAP_year1(EASY)
+        //https://leetcode.com/problems/shortest-distance-to-a-character/-->APPLE_year1(EASY)
+        //https://leetcode.com/problems/first-unique-character-in-a-string/-->APPLE_year1(EASY)
     }
 }
