95a31e27be31775a9c5f8e89b3777bfeed360ef1
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2024-12-09 11:46:49 +0530

    string and string builder complete

---
+++ src/StringAndStringBuilder/Theory.java
@@ -0,0 +1,36 @@
+package StringAndStringBuilder;
+
+public class Theory {
+    public static void main(String[] args) {
+        String name="nithin";
+        //strings are store in string pool inside heap
+        //strings are immutable in java
+        String name1="nithin";
+//        System.out.println(name1==name);
+        //above one gives true because both the strings are point to the same object
+        name1="nitin";
+//        System.out.println(name1==name);
+        //above one gives false because name1 is now pointing to different object
+        //== compares addresses
+        //whereas .equals() compares objects
+        //new object of same value can be created with new keyword
+        String name2=new String("nithin");
+//        System.out.println(name==name2);
+        //above one gives false because new address is created for name2
+//        System.out.println(name.equals(name2));
+        //above one gives true because it compares only objects
+
+        //----//
+        //formated strings
+        float a=12.12345f;
+//        System.out.printf("%.2f",a);
+        //if we are keep adding to the string then the new strings will be created everytime
+        //it can be optimized with string builder
+        StringBuilder builder=new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            builder.append(i);
+        }
+        System.out.println(builder);
+    }
+
+}
