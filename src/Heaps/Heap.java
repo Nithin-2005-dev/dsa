077ce0b87548254bab8097385f5673699364d3f3
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2025-03-31 13:37:24 +0530

    heaps revision

--- src/Heaps/Heap.java
+++ src/Heaps/Heap.java
@@ -2,7 +2,7 @@ package Heaps;
 
 import java.util.ArrayList;
 
-public class Heap<T extends Comparable<T>> {
+public class Heap<T extends Comparable<T>>{
     private ArrayList<T> list;
     public Heap(){
         list=new ArrayList<>();
@@ -12,55 +12,79 @@ public class Heap<T extends Comparable<T>> {
         list.set(first,list.get(second));
         list.set(second,temp);
     }
-    private int parent(int index){
-        return (index-1)/2;
+    private int parent(int i){
+        return (i-1)/2;
     }
-    private int left(int index){
-        return (index*2)+1;
+    private int leftChild(int i){
+        return 2*i+1;
     }
-    private int right(int index){
-        return (index*2)+2;
+    private int rightChild(int i){
+        return 2*i+2;
     }
-    public void insert(T value){
-        list.add(value);
+    public void insert(T n){
+        list.add(n);
         upHeap(list.size()-1);
     }
-    private void upHeap(int index){
-        if(index==0){
+    public T remove(int i) throws Exception{
+        if(i>=list.size()){
+            throw new Exception("enter valid index");
+        }
+        T x=list.get(i);
+        if (list.size() == 1) {
+            list.removeFirst();
+            return x;
+        }
+        list.set(i,list.removeLast());
+        if (i > 0 && list.get(i).compareTo(list.get(parent(i))) < 0) {
+            upHeap(i);
+        } else {
+            downHeap(i);
+        }
+        return x;
+    }
+    private void upHeap(int i){
+        if(i==0){
             return;
         }
-        int p=parent(index);
-        if(list.get(index).compareTo(list.get(p))<0){
-            swap(index,p);
+        int p=parent(i);
+        if(list.get(p).compareTo(list.get(i))>0){
+            swap(p,i);
             upHeap(p);
         }
     }
-    public T remove() throws Exception{
-        if (list.isEmpty()){
-            throw new Exception("Removing from an empty heap!");
-        }
-        T temp=list.get(0);
-        T last=list.remove(list.size()-1);
-        if(!list.isEmpty()) {
-            list.set(0, last);
-            downHeap(0);
-        }
-        return temp;
-    }
 
-    private void downHeap(int i) {
-        int min=i;
-        int left=left(i);
-        int right=right(i);
-        if(left<list.size() && list.get(min).compareTo(list.get(left))>0){
-            min=left;
+    private void downHeap(int i){
+       int lc=leftChild(i);
+       int rc=rightChild(i);
+        int smallest = i;
+
+        if (lc < list.size() && list.get(lc).compareTo(list.get(smallest)) < 0) {
+            smallest = lc;
         }
-        if(right<list.size() && list.get(min).compareTo(list.get(right))>0){
-            min=right;
+        if (rc < list.size() && list.get(rc).compareTo(list.get(smallest)) < 0) {
+            smallest = rc;
         }
-        if(min!=i){
-            swap(min,i);
-            downHeap(min);
+
+        if (smallest != i) {
+            swap(i, smallest);
+            downHeap(smallest);
         }
+    }
+    public T getMin(){
+        return list.getFirst();
+    }
+    public ArrayList<T> getHeap(){
+        return list;
+    }
+    public ArrayList<T> sort() throws Exception {
+        ArrayList<T> data=new ArrayList<>();
+        while (!list.isEmpty()){
+            data.add(remove(0));
+        }
+        int n=data.size();
+        for (int i = 0; i < n; i++) {
+            insert(data.get(i));
+        }
+        return data;
     }
 }
--- src/Heaps/Main.java
+++ src/Heaps/Main.java
@@ -3,9 +3,15 @@ package Heaps;
 public class Main {
     public static void main(String[] args) throws Exception {
         Heap<Integer> heap=new Heap<>();
+        heap.insert(30);
+        heap.insert(54);
         heap.insert(10);
-        heap.insert(300);
-        heap.insert(1054);
-        heap.insert(1033);
+        heap.insert(3);
+        System.out.println(heap.getMin());
+        System.out.println(heap.getHeap());
+        heap.remove(1);
+        System.out.println(heap.getMin());
+        System.out.println(heap.getHeap());
+        System.out.println(heap.sort());
     }
 }
